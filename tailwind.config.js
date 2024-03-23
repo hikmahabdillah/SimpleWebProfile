be93e5740c7963b54392c7337ab80fa806edb0d8
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-03-23 12:08:46 +0700

    add header section header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter"],
+      },
+    },
   },
   plugins: [],
 };
